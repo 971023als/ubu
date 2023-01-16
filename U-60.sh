99a54360bb59ef7b566733983135e8e831be0b33
971023als <971023als@naver.com>
2023-01-16 16:55:54 +0900

    l

---
+++ U-60.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+ 
+
+. function.sh
+
+ 
+TMP1=`SCRIPTNAME`.log
+
+> $TMP1  
+ 
+
+BAR
+
+CODE [U-60] ssh 원격접속 허용
+
+cat << EOF >> $result
+
+[양호]: 원격 접속 시 SSH 프로토콜을 사용하는 경우
+
+[취약]: 원격 접속 시 Telnet, FTP 등 안전하지 않은 프로토콜을 사용하는 경우
+
+EOF
+
+BAR
+
+
+sudo apt-get install openssh-server
+
+sudo systemctl start ssh
+
+sudo systemctl enable ssh
+
+systemctl status ssh
+
+
+
+cat $result
+
+echo ; echo 
