b4a19daf6508af963b8afbf7d09775df5420700c
971023als <971023als@naver.com>
2023-02-01 21:53:00 +0900

    l

--- U-27.sh
+++ U-27.sh
@@ -20,7 +20,7 @@ TMP1=`SCRIPTNAME`.log
 
 >$TMP1  
 
-#!/bin/bash
+# xinetd 서비스 다시 시작
 sudo sed -i 's/rpc.cmsd.*/#&/g' /etc/inetd.conf
 sudo service inetutils-inetd restart
 
@@ -28,26 +28,24 @@ sudo service inetutils-inetd restart
 # xinetd 디렉토리로 이동
 cd /etc/xinetd.d/
 
-# 디렉터리에 있는 모든 파일 나열
-files=$(ls)
-
-# 모든 파일을 반복해서 읽다
-for file in $files
-do
-  # vi 편집기에서 파일 열기
-  vi $file <<EOF
-  # "disable" 라인 찾음
-  /disable
-  # 값을 "예"로 변경합니다
-  cwyes
-  # 저장 후 종료
-  :wq
-EOF
+# vi를 사용하여 디렉토리 "/etc/xinetd.d/"에서 불필요한 모든 RPC 서비스 파일 열기
+for file in /etc/xinetd.d/*; do
+    if grep -q "disable.*=.*yes" "$file"; then
+        vi "$file"
+    fi
 done
 
-# xinetd 서비스 다시 시작
-sudo service xinetd restart
 
+
+# finger 파일 생성
+echo "service finger
+{
+socket_type = stream
+wait = no
+user = nobody
+server = /usr/sbin/in.fingerd
+disable = yes
+}" > /etc/xinetd.d/finger
 
 
 cat $result
