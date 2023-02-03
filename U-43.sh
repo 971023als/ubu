a5dcac0e09e27cffe8a41cc11cc83338f1efc20b
971023als <971023als@naver.com>
2023-02-03 21:14:15 +0900

    Update U-43.sh

--- U-43.sh
+++ U-43.sh
@@ -20,7 +20,29 @@ TMP1=`SCRIPTNAME`.log
 
 >$TMP1  
 
+# 마지막 로그인 시간 확인
+echo "Checking last login time..."
+last -a
 
+# 액세스 IP 확인
+echo "Checking access IP..."
+last -a | grep "still logged in"
+
+# 실패한 로그인 기록 확인
+echo "Checking failed login history..."
+grep "Failed password" /var/log/auth.log
+
+# 허용된 계정 정의
+allowed_accounts=("root" "bin" "daemon" "adm" "lp" "sync" "shutdown" "halt" "ubuntu" "user")
+
+# sulog에서 권한 상승 시도 여부를 점검하십시오
+echo "Checking sulog for attempted privilege escalation..."
+while read line; do
+  username=$(echo $line | awk '{print $1}')
+  if [[ ! " ${allowed_accounts[@]} " =~ " ${username} " ]]; then
+    echo "권한 상승 시도: $line"
+  fi
+done < /var/log/sulog
 
 cat $result
 
