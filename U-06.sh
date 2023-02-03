1d7f83e2bb53f9038f9ec11d58e11d3f974dad78
971023als <971023als@naver.com>
2023-02-03 13:02:13 +0900

    Update U-06.sh

--- U-06.sh
+++ U-06.sh
@@ -4,7 +4,7 @@
 
 BAR
 
-CODE [U-06] 파일 및 디렉터리 소유자 설정
+CODE [U-06] 파일 및 디렉터리 소유자 설정 @@su 말고 sudo su 해야 함 @@
 
 cat << EOF >> $result
 
@@ -20,15 +20,15 @@ TMP1=`SCRIPTNAME`.log
 
 >$TMP1  
 
-# specify the user to replace the owner with
+# 소유자를 바꿀 사용자를 지정합니다
 new_owner=user
 
-# find files and directories with nonexistent owners and groups
+# 존재하지 않는 소유자 및 그룹이 있는 파일 및 디렉터리 찾기
 results=$(find / \( -nouser -o -nogroup \) -print 2>/dev/null)
 
-# loop through each item in the results
+# 결과의 각 항목을 반복해서 살펴보다
 for item in $results; do
-  # change the owner to the specified user
+  # 소유자를 지정된 사용자로 변경
   chown $new_owner:$new_owner "$item"
 done
 
