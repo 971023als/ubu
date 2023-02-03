84211b75a2bed4d25bde01b5c33c4215a5d007b4
971023als <971023als@naver.com>
2023-02-03 16:04:35 +0900

    Update U-20.sh

--- U-20.sh
+++ U-20.sh
@@ -20,53 +20,14 @@ TMP1=`SCRIPTNAME`.log
 
 >$TMP1  
 
-# 익명 FTP 액세스가 차단되지 않았는지 확인
-if grep -q "^#.*anonymous_enable=YES" /etc/vsftpd.conf; then
-    # 익명 계정이 /etc/passwd 파일에 있는지 확인
-    if grep -q "^ftp:" /etc/passwd; then
-        # userdel 명령을 사용하여 계정 삭제
-        userdel ftp
-        echo "The anonymous FTP account has been deleted."
-    else
-        echo "The anonymous FTP account does not exist."
-    fi
-else
-    echo "Anonymous FTP access is blocked, account can't be deleted."
-fi
+# 기존 규칙
+iptables -F
 
+# 들어오는 익명 FTP 연결 차단
+iptables -A INPUT -p tcp --dport 21 -m state --state NEW -j REJECT --reject-with tcp-reset
 
-# Ubuntu 20의 proftpd.conf에서 익명 설정과 관련된 사용자 및 사용자 별칭 항목에 주석을 다는 스크립트
-
-# proftpd가 설치되어 있는지 확인
-if dpkg -s proftpd &> /dev/null; then
-    # 원래 구성 파일 백업
-    cp /etc/proftpd/proftpd.conf /etc/proftpd/proftpd.conf.bak
-
-    # 사용자 및 사용자 별칭 항목 주석 달기
-    sed -i '/^User.*anonymous/ s/^/#/' /etc/proftpd/proftpd.conf
-    sed -i '/^Useralias.*anonymous/ s/^/#/' /etc/proftpd/proftpd.conf
-
-    # proftpd 서비스 다시 시작
-    service proftpd restart
-    echo "User and Useralias entries related to anonymous settings have been annotated in proftpd.conf."
-else
-    echo "ProFTPD is not installed on this system."
-fi
-
-
-# vsftpd.conf 파일이 /etc/vsftpd/에 있는지 확인
-if [ -f "/etc/vsftpd/vsftpd.conf" ]; then
-    # anonymous_enable=YES를 anonymous_enable=NO로 바꾸기
-    sed -i 's/anonymous_enable=YES/anonymous_enable=NO/g' /etc/vsftpd/vsftpd.conf
-else
-    # vsftpd.conf 파일이 /etc/에 있는지 확인
-    if [ -f "/etc/vsftpd.conf" ]; then
-        # anonymous_enable=YES를 anonymous_enable=NO로 바꾸기
-        sed -i 's/anonymous_enable=YES/anonymous_enable=NO/g' /etc/vsftpd.conf
-    else
-        echo "vsftpd.conf file not found in /etc/vsftpd/ or /etc/"
-    fi
-fi
+# 변경 내용 저장
+iptables-save > /etc/iptables/rules.v4
 
 
 
