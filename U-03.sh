c367c23218bb0b6af8ed735025b3e4ce37f9ca1f
971023als <971023als@naver.com>
2023-02-04 12:36:59 +0900

    Update U-03.sh

--- U-03.sh
+++ U-03.sh
@@ -20,20 +20,7 @@ TMP1=`SCRIPTNAME`.log
 
 >$TMP1  
 
-# 원본 파일 백업
-cp /etc/pam.d/common-auth /etc/pam.d/common-auth.bak
-
-#  파일 수정
-echo "auth required /lib/security/pam_tally.so deny=5 unlock_time=120 no_magic_root" >> /etc/pam.d/common-auth
-echo "account required /lib/security/pam_tally.so no_magic_root reset" >> /etc/pam.d/common-auth
-
-# 변경 사항 확인
-grep -q "auth required /lib/security/pam_tally.so deny=5 unlock_time=120 no_magic_root" /etc/pam.d/common-auth
-if [ $? -eq 0 ]; then
-    OK "/etc/pam.d/common-auth 파일에 성공적으로 추가되었습니다"
-else
-    WARN "/etc/pam.d/common-auth 파일에 줄을 추가하는 동안 오류가 발생했습니다."
-fi
+ 
 
 
 
