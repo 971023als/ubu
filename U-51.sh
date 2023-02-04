9d9d1feb3d6d9c0b5acb9e88f13f0996717b9512
971023als <971023als@naver.com>
2023-02-05 02:08:05 +0900

    Update U-51.sh

--- U-51.sh
+++ U-51.sh
@@ -32,12 +32,13 @@ BAR
 TMP1=`SCRIPTNAME`.log
 
 > $TMP1
+
 necessary_groups=("root" "sudo" "sys" "adm" "wheel" 
 "daemon" "bin" "lp" "dbus" "rpc" "rpcuser" "haldaemon" 
 "apache" "postfix" "gdm" "adiosl" "mysql" "cubrid"
-"sync" "messagebus" "syslog" "avahi" "whoopsie"
+ "messagebus" "syslog" "avahi" "whoopsie"
 "colord" "systemd-network" "systemd-resolve"
-"systemd-timesync" "mysql")
+"systemd-timesync" "mysql" "sync" "user")
 
 all_groups=$(getent group | awk -F: '{print $1}')
 
