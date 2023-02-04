e7e3b12726dddc77d5ff38095f1a8d1aa03efe0f
971023als <971023als@naver.com>
2023-02-04 11:35:19 +0900

    Update U-43.sh

--- U-43.sh
+++ U-43.sh
@@ -20,29 +20,45 @@ TMP1=`SCRIPTNAME`.log
 
 >$TMP1  
 
-# 마지막 로그인 시간 확인
-echo "Checking last login time..."
-last -a
 
-# 액세스 IP 확인
-echo "Checking access IP..."
-last -a | grep "still logged in"
+# 현재 날짜 및 시간 저장
+current_date_time=$(date +"%Y-%m-%d %T")
 
-# 실패한 로그인 기록 확인
-echo "Checking failed login history..."
-grep "Failed password" /var/log/auth.log
+# su를 사용한 사용자의 사용자 이름 가져오기
+username=$(last | awk '/su/ {print $1}')
 
-# 허용된 계정 정의
-allowed_accounts=("root" "bin" "daemon" "adm" "lp" "sync" "shutdown" "halt" "ubuntu" "user")
+# sulog 파일에 로그 추가
+echo "$current_date_time $username used su" >> /var/log/sulog
 
-# sulog에서 권한 상승 시도 여부를 점검하십시오
-echo "Checking sulog for attempted privilege escalation..."
-while read line; do
-  username=$(echo $line | awk '{print $1}')
-  if [[ ! " ${allowed_accounts[@]} " =~ " ${username} " ]]; then
-    echo "권한 상승 시도: $line"
-  fi
-done < /var/log/sulog
+
+# 실패한 최대 로그인 시도 횟수 설정
+max_attempts=5
+
+# 실패한 로그인 이벤트에서 사용자 이름 가져오기
+username=$(grep "Failed password" /var/log/auth.log | tail -n 1 | awk '{print $(NF-5)}')
+
+# 사용자 이름에 대한 로그인 시도 실패 횟수 카운트
+attempts=$(grep "Failed password" /var/log/auth.log | grep "$username" | wc -l)
+
+# 실패한 시도 횟수가 최대값보다 큰지 확인
+if [ "$attempts" -gt "$max_attempts" ]; then
+  # 현재 날짜 및 시간 저장
+  current_date_time=$(date +"%Y-%m-%d %T")
+
+  # auth_logs 파일에 로그 저장
+  echo "$current_date_time $username has exceeded the maximum number of failed login attempts ($max_attempts)" >> /var/log/auth_logs
+fi
+
+
+# 로그인 거부 이벤트에서 사용자 이름 가져오기
+username=$(grep "authentication failure" /var/log/auth.log | tail -n 1 | awk '{print $(NF-5)}')
+
+# 현재 날짜 및 시간 저장
+current_date_time=$(date +"%Y-%m-%d %T")
+
+# auth_logs 파일에 로그 저장
+echo "$current_date_time Login rejection for user $username" >> /var/log/auth_logs
+
 
 cat $result
 
