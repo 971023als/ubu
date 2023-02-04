d5b3b5f1ee49f5de725752865df2d8e728be8afd
971023als <971023als@naver.com>
2023-02-04 12:09:39 +0900

    Update U-18.sh

--- U-18.sh
+++ U-18.sh
@@ -20,7 +20,11 @@ TMP1=`SCRIPTNAME`.log
 
 >$TMP1  
 
+# 포트 22(SSH)에서 192.168.0.1로부터의 연결 허용
+echo "sshd: 192.168.0.1" >> /etc/hosts.allow
 
+# 포트 22(SSH)의 다른 모든 IP 주소에서 연결 거부
+echo "sshd: ALL" >> /etc/hosts.deny
 
 
 
