dee5d56b529d3b30127690a838f27fff8086e245
971023als <971023als@naver.com>
2023-02-04 22:35:54 +0900

    Update U-33.sh

--- U-33.sh
+++ U-33.sh
@@ -27,16 +27,16 @@ sudo chkconfig named off
 #DNS 서비스가 중지되었는지 확인합니다
 
 if [ $(sudo service named status | grep -c "is running") -eq 0 ]; then
-OK "DNS 서비스가 중지되었습니다."
+    OK "DNS 서비스가 중지되었습니다."
 else
-WARN "DNS 서비스가 아직 실행 중입니다."
+    WARN "DNS 서비스가 아직 실행 중입니다."
 fi
 
 #부팅 시 DNS 서비스가 사용되지 않도록 설정되었는지 확인
 if [ "$(sudo chkconfig --list named | grep -c "3:off")" -eq 1 ]; then
-OK "DNS 서비스가 부팅 시 비활성화됩니다."
+    OK "DNS 서비스가 부팅 시 비활성화됩니다."
 else
-WARN "부팅 시 DNS 서비스가 사용되지 않도록 설정되지 않았습니다."
+    WARN "부팅 시 DNS 서비스가 사용되지 않도록 설정되지 않았습니다."
 fi
 
 
