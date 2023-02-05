ca9db6170ed16e26048ade42f4f5686e8512aabc
971023als <971023als@naver.com>
2023-02-05 11:54:02 +0900

    Update U-06.sh

--- U-06.sh
+++ U-06.sh
@@ -20,6 +20,23 @@ TMP1=`SCRIPTNAME`.log
 
 >$TMP1  
 
+# 소유자가 없는 모든 파일 및 디렉터리 찾기
+unowned=$(find . -nouser)
+
+#  소유하지 않은 파일 또는 디렉터리가 있는지 확인하십시오
+if [ -z "$unowned" ]; then
+  OK "소유되지 않은 파일 또는 디렉터리를 찾을 수 없습니다."
+fi
+
+new_owner="root"
+
+# Change the owner of each unowned file or directory
+for item in $unowned; do
+  sudo chown "$new_owner" "$item"
+  INFO "$item 의 소유자가 $new_owner 로 변경됨"
+done
+
+OK "소유되지 않은 파일 및 디렉터리의 소유권 변경을 완료했습니다."
 
 
 
