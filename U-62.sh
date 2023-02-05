e7be27716707df960b5b0696260f08e3e478a1a6
971023als <971023als@naver.com>
2023-02-05 15:05:13 +0900

    Update U-62.sh

--- U-62.sh
+++ U-62.sh
@@ -27,25 +27,26 @@ TMP1=`SCRIPTNAME`.log
 # 계정 이름 정의
 account_name="ftp"
 
-# /etc/passwd 파일에서 계정에 해당하는 줄 찾기
+# 계정에 해당하는 /etc/passwd 파일에서 줄 찾기
 line=$(grep "^$account_name:" /etc/passwd)
 
-# 계정을 찾을 수 없으면 메시지를 표시하고 종료합니다
+# 계정을 찾을 수 없는 경우 메시지 표시 및 종료
 if [ -z "$line" ]; then
-  INFO "/etc/passwd에 $account_name 계정이 없습니다."
+  echo "/etc/passwd 파일에 $account_name 계정이 없습니다."
 fi
 
 # 현재 로그인 셸 추출
 current_shell=$(echo $line | cut -d: -f7)
 
-#  현재 셸이 이미 /bin/false로 설정되어 있는지 확인하십시오
+# 현재 셸이 이미 /bin/false로 설정되어 있는지 확인하십시오
 if [ "$current_shell" != "/bin/false" ]; then
   # 현재 셸을 /bin/false로 변경합니다
-  new_line=$(echo "$line" | sed "s,$current_shell,/bin/false,")
+  new_line=$(echo "$line" | sed "s#$current_shell#/bin/false#")
 
   # /etc/passwd 파일 업데이트
   sudo sed -i "s#$line#$new_line#" /etc/passwd
 fi
+
 
 
 cat $result
