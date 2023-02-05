5610c66591d6e7f584f46d7b297d2a702bfda649
971023als <971023als@naver.com>
2023-02-05 15:12:47 +0900

    Update U-52.sh

--- U-52.sh
+++ U-52.sh
@@ -35,45 +35,48 @@ TMP1=`SCRIPTNAME`.log
 
 > $TMP1
 
-# 고유 사용자 ID(UID) 목록 가져오기
+# 고유 UID 목록 가져오기
 unique_uids=$(cat /etc/passwd | awk -F: '{print $3}' | sort | uniq)
 
 # 각 UID를 반복합니다
 for uid in $unique_uids; do
-  # 동일한 UID를 가진 사용자 계정 수 계산
+  # 동일한 UID를 가진 계정 수 계산
   count=$(grep ":$uid:" /etc/passwd | wc -l)
 
-  # 동일한 UID를 가진 사용자 계정이 둘 이상인 경우 다음 UID로 건너뜁니다
-  if [ $count -gt 1 ]; then
-    # 동일한 UID를 가진 사용자 계정 목록 가져오기
-    users=$(grep ":$uid:" /etc/passwd | awk -F: '{print $1}')
-
-    #  각 사용자 계정 루프
-    for user in $users; do
-      # 동일한 UID를 가진 첫 번째 사용자 계정 건너뛰기
-      if [ $count -eq $uid ]; then
-        count=$((count - 1))
-        continue
-      fi
-
-      # 새 UID 생성
-      new_uid=$((new_uid + 1))
-
-      # Check if new_uid already exists
-      while grep -q ":$new_uid:" /etc/passwd; do
-        # Increment new_uid
-        new_uid=$((new_uid + 1))
-      done
-
-
-      # 사용자 계정의 UID 변경
-      sudo usermod --uid $new_uid $user
-
-      # 카운트를 줄입니다
-      count=$((count - 1))
-    done
+  # 동일한 UID를 가진 계정이 하나만 있는 경우 건너뛰기
+  if [ $count -le 1 ]; then
+    continue
   fi
+
+  # 동일한 UID를 가진 계정 목록 가져오기
+  users=$(grep ":$uid:" /etc/passwd | awk -F: '{print $1}')
+
+  # 각 계정을 반복합니다
+  for user in $users; do
+    # 동일한 UID를 가진 첫 번째 계정 건너뛰기
+    if [ $count -eq $uid ]; then
+      count=$((count - 1))
+      continue
+    fi
+
+    # 현재 uid로 new_uid 초기화
+    new_uid=$uid
+
+    # new_uid가 이미 있는지 확인하십시오
+    while grep -q ":$new_uid:" /etc/passwd; do
+      # new_uid 증가
+      new_uid=$((new_uid + 1))
+    done
+
+    # 계정의 UID 변경
+    sudo usermod --uid $new_uid $user
+
+    # 감산수
+    count=$((count - 1))
+  done
 done
+
+
  
 
 cat $result
