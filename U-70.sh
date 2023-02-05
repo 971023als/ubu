d1a9ff8e452ad7b6f10f7cbd414ab4586419f6d9
971023als <971023als@naver.com>
2023-02-05 12:11:14 +0900

    Update U-70.sh

--- U-70.sh
+++ U-70.sh
@@ -29,7 +29,23 @@ TMP1=`SCRIPTNAME`.log
 sudo service sendmail stop
 
 
+CONF_FILE=/etc/postfix/main.cf
 
+if grep -q "smtpd_recipient_restrictions" "$CONF_FILE"; then
+  if grep -q "noexpn" "$CONF_FILE" && grep -q "novfy" "$CONF_FILE"; then
+    OK "smtpd_snmp_recommunications에 noexpn 및 novfy가 이미 있습니다."
+  else
+    # 원래 구성 파일 백업
+    cp "$CONF_FILE" "$CONF_FILE.bak"
+
+    # 'noexpn' 및 'novfy'를 smtpd_transmissions 줄에 추가합니다
+    sed -i '/smtpd_recipient_restrictions/ s/$/ noexpn, novfy/' "$CONF_FILE"
+
+    INFO "smtpd_messagions에 noexpn 및 novfy가 추가되었습니다."
+  fi
+else
+  WARN "구성 파일에서 esxd_disclusions를 찾을 수 없습니다."
+fi
     
 
 cat $result
