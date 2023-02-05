a29316276cd9993d1d9f75747ce9406228469ff2
971023als <971023als@naver.com>
2023-02-05 22:52:46 +0900

    Update U-44.sh

--- U-44.sh
+++ U-44.sh
@@ -16,6 +16,13 @@ EOF
 
 BAR
 
+# 루트 계정과 동일한 UID를 가진 계정이 있는지 확인하십시오
+if ! grep -q ":$ROOT_UID:" /etc/passwd; then
+  # 루트 계정과 동일한 UID를 가진 계정이 없는 경우 작업 수행
+  INFO "루트 계정과 동일한 UID를 가진 계정이 없습니다."
+  # 예제 작업: 루트 계정과 동일한 UID를 가진 새 사용자 추가
+  sudo useradd -u $ROOT_UID newuser
+fi
 
 cat $result
 
