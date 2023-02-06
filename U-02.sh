37790d1296bfebc0ff4832718dee785b870f9486
971023als <971023als@naver.com>
2023-02-06 13:24:40 +0900

    Update U-02.sh

--- U-02.sh
+++ U-02.sh
@@ -36,16 +36,16 @@ min_numeric=2
 # 최소 특수 문자 수 설정
 min_special=2
 
-# pam_cracklib이 있는지 확인합니다.so는 /etc/syslog.d/system-auth에 있습니다
-if grep -q "pam_cracklib.so" /etc/pam.d/system-auth; then
+# pam_cracklib이 있는지 확인합니다.so는 /etc/syslog.d/common-auth에 있습니다
+if grep -q "pam_cracklib.so" /etc/pam.d/common-auth; then
   # 기존 pam_cracklib을 업데이트합니다.
-  sed -i "s/\(pam_cracklib.so.*\)ucredit=[0-9]*\(.*\)/\1ucredit=$min_upper\2/" /etc/pam.d/system-auth
-  sed -i "s/\(pam_cracklib.so.*\)lcredit=[0-9]*\(.*\)/\1lcredit=$min_lower\2/" /etc/pam.d/system-auth
-  sed -i "s/\(pam_cracklib.so.*\)dcredit=[0-9]*\(.*\)/\1dcredit=$min_numeric\2/" /etc/pam.d/system-auth
-  sed -i "s/\(pam_cracklib.so.*\)ocredit=[0-9]*\(.*\)/\1ocredit=$min_special\2/" /etc/pam.d/system-auth
+  sed -i "s/\(pam_cracklib.so.*\)ucredit=[0-9]*\(.*\)/\1ucredit=$min_upper\2/" /etc/pam.d/common-auth
+  sed -i "s/\(pam_cracklib.so.*\)lcredit=[0-9]*\(.*\)/\1lcredit=$min_lower\2/" /etc/pam.d/common-auth
+  sed -i "s/\(pam_cracklib.so.*\)dcredit=[0-9]*\(.*\)/\1dcredit=$min_numeric\2/" /etc/pam.d/common-auth
+  sed -i "s/\(pam_cracklib.so.*\)ocredit=[0-9]*\(.*\)/\1ocredit=$min_special\2/" /etc/pam.d/common-auth
 else
   # pam_cracklib을 사용하여 새 줄을 추가합니다.
-  echo "password    required    pam_cracklib.so retry=3 ucredit=$min_upper lcredit=$min_lower dcredit=$min_numeric ocredit=$min_special" >> /etc/pam.d/system-auth
+  echo "password    required    pam_cracklib.so retry=3 ucredit=$min_upper lcredit=$min_lower dcredit=$min_numeric ocredit=$min_special" >> /etc/pam.d/common-auth
 fi
 
 
