14e7bd0176ccc91be6d070b9d6846f2fe146e488
971023als <971023als@naver.com>
2023-02-06 22:52:16 +0900

    Update U-57.sh

--- U-57.sh
+++ U-57.sh
@@ -27,22 +27,6 @@ TMP1=`SCRIPTNAME`.log
 > $TMP1
 
 
-cat /etc/passwd | awk -F ':' '{print $6}' | while read home_dir; do
-  # 홈 디렉토리에 대한 소유권 및 사용 권한 정보를 가져옵니다
-  ls -ld "$home_dir" | while read permissions owner group; do
-    # 홈 디렉토리 소유자로 변경
-    username=$(basename "$home_dir")
-    sudo -u "$username" bash << EOF
-      cd "$home_dir"
-      # 다른 사용자에 대한 쓰기 권한 제거
-      sudo chmod o-w .
-EOF
-  done
-done
-
-
-
-
 cat $result
 
 echo ; echo 
