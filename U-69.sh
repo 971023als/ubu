2f461b10e7c6bf86901bd847098cf30136ddf52e
971023als <971023als@naver.com>
2023-02-06 14:22:48 +0900

    Update U-69.sh

--- U-69.sh
+++ U-69.sh
@@ -27,27 +27,8 @@ TMP1=`SCRIPTNAME`.log
 
 filename="/etc/exports"
 
-if [ ! -e "$filename" ]; then
-  INFO "$filename 가 존재하지 않습니다"
-fi
-
 sudo chown root "$filename"
 sudo chmod 644 "$filename"
-
-owner=$(stat -c '%U' "$filename")
-permission=$(stat -c '%a' "$filename")
-
-if [ "$owner" == "root" ]; then
-  OK "$filename의 소유자가 루트로 설정되었습니다."
-else
-  WARN "$filename의 소유자를 루트로 설정하지 못했습니다."
-fi
-
-if [ $(expr "$permission" \<= 644) -eq 1 ]; then
-  OK "$filename의 권한이 644 이하로 설정되었습니다."
-else
-  WARN "$filename의 권한을 644 이하로 설정하지 못했습니다."
-fi
 
 
 cat $result
