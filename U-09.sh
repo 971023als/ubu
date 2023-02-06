9e80774162e7b8e1c5b976d5f138a055d69ea303
971023als <971023als@naver.com>
2023-02-06 13:55:13 +0900

    ok

--- U-09.sh
+++ U-09.sh
@@ -16,37 +16,11 @@ EOF
 
 BAR
 
-TMP1=`SCRIPTNAME`.log
+# 파일 소유자를 "root" 사용자와 "root" 그룹으로 변경
+sudo chown root:root /etc/hosts
 
->$TMP1  
-
-HOSTS_FILE="/etc/hosts"
-
-if [ ! -f $HOSTS_FILE ]; then
-  OK "$HOSTs_FILE이 존재하지 않습니다"
-fi
-
-# 소유자를 루트로 설정
-sudo chown root $HOSTS_FILE
-
-# 권한을 600으로 설정
-sudo chmod 600 $HOSTS_FILE
-
-#  변경 사항 확인
-owner=$(stat -c '%U' $HOSTS_FILE)
-permissions=$(stat -c '%a' $HOSTS_FILE)
-
-if [ "$owner" != "root" ]; then
-  WARN "$HOSTs_FILE 의 소유자를 루트로 설정하지 못했습니다."
-else
-  OK "$HOSTs_FILE 이 이제 루트에 의해 소유됩니다."
-fi
-
-if [ "$permissions" != "600" ]; then
-  WARN "$HOSTs_FILE 의 사용 권한을 600으로 설정하지 못했습니다."
-else
-  OK "$HOSTs_FILE 에 권한 600이 있습니다."
-fi
+# 파일의 권한을 600으로 설정
+sudo chmod 600 /etc/hosts
 
 
 
