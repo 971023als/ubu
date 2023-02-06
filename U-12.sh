f3d40efa28c66383e9b5b312ad21b4b797bbd6a0
971023als <971023als@naver.com>
2023-02-06 14:13:07 +0900

    Update U-12.sh

--- U-12.sh
+++ U-12.sh
@@ -18,23 +18,11 @@ EOF
 
 BAR
 
-file="/etc/services"
+# 파일 소유자를 "root" 사용자와 "root" 그룹으로 변경
+sudo chown root:root /etc/services
 
-# 소유권확인
-owner=$(stat -c '%U' "$file")
-if [ "$owner" != "root" ] && [ "$owner" != "bin" ] && [ "$owner" != "sys" ]; then
-  WARN "$file 의 소유자가 root, bin 또는 sys가 아닙니다. 그것은 $owner 가 소유하고 있다."
-else
-  OK "$file 의 소유자는 root, bin 또는 sys입니다."
-fi
-
-# 권한 확인
-permissions=$(stat -c '%a' "$file")
-if [ "$permissions" -gt 644 ]; then
-  WARN "$file의 권한이 644보다 큽니다. 그들은 $permissions 으로 설정되어 있습니다."
-else
-  OK "$file의 권한이 644 이하입니다."
-fi
+# 파일의 권한을 644로 설정
+sudo chmod 644 /etc/services
 
 
 cat $result
