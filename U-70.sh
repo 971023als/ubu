cb0bdc98196c9f4afff8b1f9c9dcb148957dab97
971023als <971023als@naver.com>
2023-02-06 14:21:26 +0900

    Update U-70.sh

--- U-70.sh
+++ U-70.sh
@@ -20,28 +20,15 @@ EOF
 
 BAR
 
+# 송신 메일 프로세스의 PID 가져오기
+pid=$(ps -ef | grep sendmail | awk '{print $2}')
 
+# PID를 사용하여 송신 메일 프로세스 중지
+kill -9 $pid
 
-# SMTP 서비스 중지
-sudo service sendmail stop
+# /etc/rc2.d에서 sendmail init 스크립트 이동
+mv /etc/rc2.d/S88sendmail /etc/rc2.d/S88sendmail_bak
 
-CONF_FILE=/etc/postfix/main.cf
-
-if grep -q "smtpd_recipient_restrictions" "$CONF_FILE"; then
-  if grep -q "noexpn" "$CONF_FILE" && grep -q "novfy" "$CONF_FILE"; then
-    OK "smtpd_snmp_recommunications에 noexpn 및 novfy가 이미 있습니다."
-  else
-    # 원래 구성 파일 백업
-    cp "$CONF_FILE" "$CONF_FILE.bak"
-
-    # 'noexpn' 및 'novfy'를 smtpd_transmissions 줄에 추가합니다
-    sed -i '/smtpd_recipient_restrictions/ s/$/ noexpn, novfy/' "$CONF_FILE"
-
-    INFO "smtpd_messagions에 noexpn 및 novfy가 추가되었습니다."
-  fi
-else
-  INFO "구성 파일을 찾을 수 없습니다."
-fi
     
 
 cat $result
