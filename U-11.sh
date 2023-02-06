d8ac917550cd7adf487212caa01c162c437262da
971023als <971023als@naver.com>
2023-02-06 14:09:39 +0900

    Update U-11.sh

--- U-11.sh
+++ U-11.sh
@@ -4,14 +4,14 @@
 
 BAR
 
-CODE [U-11] /etc/syslog.conf 파일 소유자 및 권한 설정		
+CODE [U-11] /etc/rsyslog.conf 파일 소유자 및 권한 설정		
 
 cat << EOF >> $result
 
-[양호]: /etc/syslog.conf 파일의 소유자가 root(또는 bin, sys)이고, 권한이 640 이
+[양호]: /etc/rsyslog.conf 파일의 소유자가 root(또는 bin, sys)이고, 권한이 640 이
 하인 경우
 
-[취약]: /etc/syslog.conf 파일의 소유자가 root(또는 bin, sys)가 아니거나, 권한이 
+[취약]: /etc/rsyslog.conf 파일의 소유자가 root(또는 bin, sys)가 아니거나, 권한이 
 640 이하가 아닌 경우
 
 EOF
@@ -22,7 +22,7 @@ BAR
 sudo chown root:root /etc/rsyslog.conf
 
 # 파일의 권한을 644로 설정
-sudo chmod 644 /etc/rsyslog.conf
+sudo chmod 640 /etc/rsyslog.conf
 
 cat $result
 
