92f65bba2d467de2ee25a4819dc323fc9b304fc0
971023als <971023als@naver.com>
2023-02-07 15:43:23 +0900

    Update U-49.sh

--- U-49.sh
+++ U-49.sh
@@ -34,13 +34,91 @@ for user in $user_list; do
  # 사용자 셸을 /bin/false로 변경합니다
   usermod -s /bin/false $user
   if [ $? -eq 0 ]; then
-    echo "Login is now impossible for user: $user"
+    INFO "이제 사용자: $user 에 대해 로그인할 수 없습니다."
   else
-    echo "Could not make login impossible for user: $user"
+    OK "사용자: $user 에 대해 로그인할 수 없습니다."
   fi
 done
 
+# 기본 계정 목록 지정
+default_accounts=(
+  "root"
+  "bin"
+  "daemon"
+  "adm"
+  "lp"
+  "sync"
+  "shutdown"
+  "halt"
+  "ubuntu"
+  "user"
+  "messagebus"
+  "syslog"
+  "avahi"
+  "kernoops"
+  "whoopsie"
+  "colord"
+  "systemd-network"
+  "systemd-resolve"
+  "systemd-timesync"
+  "mysql"
+  "dbus"
+  "rpc"
+  "rpcuser"
+  "haldaemon"
+  "apache"
+  "postfix"
+  "gdm"
+  "adiosl"
+  "cubrid"
+  "sys"
+  "games"
+  "man"
+  "news"
+  "uucp"
+  "proxy"
+  "www-data"
+  "backup"
+  "list"
+  "irc"
+  "gnarts"
+  "nobody"
+  "_apt"
+  "tss"
+  "uuidd"
+  "tcpdump"
+  "avahi-autoipad"
+  "usbmux"
+  "rtkit"
+  "dnsmasq"
+  "cups-pk-helper"
+  "speech-dispatcher"
+  "saned"
+  "nm-openvpn"
+  "hplip"
+  "geoclue"
+  "pulse"
+  "gnone-initial-setup"
+  "systmd-coredump"
+  "fwupd-refresh"
+)
 
+# 셸이 bash로 설정된 사용자 목록을 /etc/passwd에서 가져옵니다
+user_list=$(cat /etc/passwd | grep bash | awk -F: '{print $1}')
+
+# 사용자 목록을 순환
+for user in $user_list; do
+  # 사용자가 기본 계정인지 확인합니다
+  if echo "$default_accounts" | grep -qw "$user"; then
+    # 아무것도 하지마
+    :
+  else
+    INFO "기본이 아닌 계정 제거 중: $user"
+    sudo userdel "$user"
+  fi
+done
+
+OK "기본이 아닌 모든 계정이 제거되었습니다."
 
  
 cat $result
