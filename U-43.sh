e38b746057bdefb4bbdc55017896be124cac3181
971023als <971023als@naver.com>
2023-02-08 13:02:06 +0900

    Update U-43.sh

--- U-43.sh
+++ U-43.sh
@@ -30,23 +30,13 @@ username=$(last | awk '/su/ {print $1}')
 echo "$current_date_time $username used su" >> /var/log/sulog
 
 
-# 실패한 최대 로그인 시도 횟수 설정
-max_attempts=5
+# 반복적인 로그인 실패에 대한 임계값 설정
+threshold=5
 
-# 실패한 로그인 이벤트에서 사용자 이름 가져오기
-username=$(grep "Failed password" /var/log/auth.log | tail -n 1 | awk '{print $(NF-5)}')
+# 로그 파일에서 반복되는 로그인 실패 횟수를 가져옵니다
+count=$(grep -c "Failed password" /var/log/auth.log)
 
-# 사용자 이름에 대한 로그인 시도 실패 횟수 카운트
-attempts=$(grep "Failed password" /var/log/auth.log | grep "$username" | wc -l)
-
-# 실패한 시도 횟수가 최대값보다 큰지 확인
-if [ "$attempts" -gt "$max_attempts" ]; then
-  # 현재 날짜 및 시간 저장
-  current_date_time=$(date +"%Y-%m-%d %T")
-
-  # auth_logs 파일에 로그 저장
-  echo "$current_date_time $username has exceeded the maximum number of failed login attempts ($max_attempts)" >> /var/log/auth_logs
-fi
+echo "Threshold of $threshold repeated login failures reached. Total failures: $count" >> /var/log/auth.log
 
 
 # 로그인 거부 이벤트에서 사용자 이름 가져오기
