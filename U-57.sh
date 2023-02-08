a6df395b39e0cf888e81808ae47293a08ba0b367
971023als <971023als@naver.com>
2023-02-08 16:09:56 +0900

    Update U-57.sh

--- U-57.sh
+++ U-57.sh
@@ -20,23 +20,30 @@ TMP1=`SCRIPTNAME`.log
 
 > $TMP1
 
-# /etc/passwd에서 홈 디렉토리 가져오기
-while read home_dir; do
-  # 홈 디렉토리의 소유권 및 사용 권한 가져오기
-  while read permissions owner group; do
-    # 홈 디렉토리 소유자가 사용자 이름과 일치하는지 확인합니다
-    username="$username:$(id -gn "$username")"
+# /etc/passwd의 각 줄을 반복합니다
+while read line; do
+  # 구분 기호로 ':'를 사용하여 줄을 필드로 분할
+  fields=($(echo $line | tr ':' ' '))
+  username=${fields[0]}
+  home_dir=${fields[5]}
+  owner=$(ls -ld "$home_dir" | awk '{print $3}')
+  
+  # 홈 디렉토리 소유자가 사용자 이름과 일치하는지 확인합니다
+  if [ "$owner_group" != "$username:$username" ]; then
+    INFO "$homeedir의 소유권을 $username 으로 변경:$username"
+    sudo chown "$username:$username" "$homedir"
+  fi
+done < /etc/passwd
 
-    # 홈 디렉토리 소유자가 사용자 이름과 일치하도록 변경
-    if [ "$owner" != "$username" ]; then
-      sudo chown "$username" "$home_dir"
-    fi
-    
-    # 최종 사용자에 대한 쓰기 권한 제거
-    sudo chmod o-w "$home_dir"
+
+# /etc/passwd에서 홈 디렉토리 목록 가져오기
+while read home_dir; do
+  # 홈 디렉토리의 소유자 및 그룹 가져오기
+  while read permissions owner group; do
+    # 소유자 그룹 등에 쓰기 권한 부여
+    sudo chmod g+w,o+w "$home_dir"
   done < <(ls -ld "$home_dir")
 done < <(cat /etc/passwd | awk -F ':' '{print $6}')
-
 
 cat $result
 
