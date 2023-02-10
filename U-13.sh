c8ecb5e98e0ea3d5e99b82204151243b24e89612
971023als <971023als@naver.com>
2023-02-10 14:43:57 +0900

    Update U-13.sh

--- U-13.sh
+++ U-13.sh
@@ -23,7 +23,7 @@ do
   # 각 홈 디렉토리에 대해 반복
   if [ -d "$home" ]; then
     # 홈 디렉토리에서 기본 실행 파일의 사용 권한 정보를 가져옵니다
-    main_exec=$(find "$home" -type f \( -perm -04000 -o -perm -02000 \) -xdev -exec ls -al {} \;)
+    main_exec=$(find "$home" -type f \( -perm -04000 -o -perm -02000 \) -exec ls -al {} \;)
 
     # 주 실행 파일이 존재하는 경우
     if [ -n "$main_exec" ]; then
