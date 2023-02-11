fc1f11dba6bdd1c25ef65ae0b3fa87c17fc54ad6
971023als <971023als@naver.com>
2023-02-12 03:11:35 +0900

    Update U-02.sh

--- U-02.sh
+++ U-02.sh
@@ -29,16 +29,7 @@ echo "PASS_MIN_LEN $pass" >> "$DEF_FILE"
 PAM_FILE="/etc/pam.d/common-auth"
 EXPECTED_OPTIONS="password    requisite    pam_cracklib.so try_first_pass restry=3 minlen=8 lcredit=-1 ucredit=-1 dcredit=-1 ocredit=-1"
 
-
-if [ -f "$PAM_FILE" ]; then
-    if grep -q "$EXPECTED_OPTIONS" "$PAM_FILE" ; then
-        OK " "$PAM_FILE" 에 $EXPECTED_OPTIONS 없음  "
-    else
-        echo ""$EXPECTED_OPTIONS"" >> "$PAM_FILE"
-        INFO " "$PAM_FILE" 에 설정되었습니다  "
-    fi
-else
-    INFO " "$PAM_FILE" 못 찾음"
+echo ""$EXPECTED_OPTIONS"" >> "$PAM_FILE"
 
 cat $result
 
