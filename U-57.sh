419c939ba3275c1588170f8968cadfa14099c005
971023als <971023als@naver.com>
2023-02-15 07:24:31 +0900

    Update U-57.sh

--- U-57.sh
+++ U-57.sh
@@ -20,18 +20,19 @@ BAR
 output=$(cat /etc/passwd | awk -F ':' '{print $6}')
 
 # 출력을 배열로 분할
-arr=($output)
+IFS=$'\n' read -d '' -r -a arr <<< "$output"
 
 # 배열을 반복하여 각 홈 디렉토리 확인
 for line in "${arr[@]}"
 do
+  # 디렉토리에 다른 사용자에 대한 쓰기 권한이 있는지 확인합니다
+  if [ -w "$line" ] && ! [[ -O "$line" ]]; then
   permissions=$(ls -ld $line | awk '{print $1}')
   owner=$(ls -ld $line | awk '{print $3}')
   group=$(ls -ld $line | awk '{print $4}')
-  if [[ $permissions == *"w"* ]] && [[ $owner != *$group* ]]; then
-    INFO "소유권 및 권한 변경: $line($owner 및 그룹 $group 소유)"
-    chown $owner:$group $line
-    chmod 750 $line
+  INFO "$line($owner 및 group $group 소유)에 대한 소유권 및 권한 변경"
+  chown $owner:$group $line
+  chmod 750 $line
   fi
 done
 
